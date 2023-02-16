11f8fa979e1820c398d5f106e675bebf783fa049
aj4941 <aj4941@naver.com>
2023-02-16 15:41:47 +0900

    albufindByAlbumName 메서드 추가

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -3,10 +3,12 @@ package com.squarecross.photoalbum.service;
 import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.repository.AlbumRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AssertionsKt;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,5 +29,31 @@ class AlbumServiceTest {
 
         Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
         assertEquals("테스트", resAlbum.getAlbumName());
+    }
+    @Test
+    void getAlbumByName() {
+        // given
+        Album hasAlbum = new Album();
+        hasAlbum.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(hasAlbum);
+
+        // when
+        Album findAlbum = albumService.getAlbumByName("테스트");
+
+        // then
+        assertThat(findAlbum).isEqualTo(savedAlbum);
+
+
+
+        // given
+        Album hasNotAlbum = new Album();
+        hasNotAlbum.setAlbumName("테스트2");
+
+        // when
+        Album savedAlbum2 = albumRepository.save(hasNotAlbum);
+
+        // then
+        assertThrows(EntityNotFoundException.class,
+                () -> albumService.getAlbumByName("xxxxx"));
     }
 }
