809ad86e5c746fb6fc071eda19ccb5dfd7ccb25b
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-21 17:43:18 +0900

    PhotoDto, PhotoMapper 설정

--- src/main/java/com/squarecross/photoalbum/dto/PhotoDto.java
+++ src/main/java/com/squarecross/photoalbum/dto/PhotoDto.java
@@ -1,7 +1,11 @@
 package com.squarecross.photoalbum.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter @Setter
 public class PhotoDto {
     private Long photoId;
     private String fileName;
--- src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
+++ src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
@@ -4,9 +4,16 @@ import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.PhotoDto;
 
 public class PhotoMapper {
+
     public static PhotoDto convertToDto(Photo photo) {
         PhotoDto photoDto = new PhotoDto();
-
+        photoDto.setPhotoId(photo.getPhotoId());
+        photoDto.setFileName(photo.getFileName());
+        photoDto.setFileSize(photo.getFileSize());
+        photoDto.setUploadedAt(photo.getUploadedAt());
+        photoDto.setThumbUrl(photo.getThumbUrl());
+        photoDto.setOriginalUrl(photo.getOriginalUrl());
+        photoDto.setAlbumId(photo.getAlbum().getAlbumId());
         return photoDto;
     }
 }
