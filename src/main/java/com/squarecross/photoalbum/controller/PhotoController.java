bf1b64cce2219c6b0689af5300a668bce899c46e
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-22 09:51:40 +0900

    사진 업로드 컨트롤러 작성

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -1,15 +1,16 @@
 package com.squarecross.photoalbum.controller;
 
+import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.repository.PhotoRepository;
 import com.squarecross.photoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/albums/{albumId}/photos")
@@ -23,4 +24,11 @@ public class PhotoController {
         PhotoDto photoDto = photoService.getPhoto(photoId);
         return new ResponseEntity<>(photoDto, HttpStatus.OK);
     }
+
+//    @PostMapping
+//    public ResponseEntity<List<Photo>> uploadPhotos(
+//            @PathVariable("albumId") Long albumId,
+//            @RequestParam("photos") MultipartFile[] files) {
+            // photos 입력 필드로 업로드된 파일이 files 배열 객체 안에 저장
+//    }
 }
